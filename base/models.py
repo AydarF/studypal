00589990c9464703ddb37a0f4299f8869ebeb580
Aydar Fayzullin <faidarik@mail.ru>
2022-01-23 13:59:38 -0500

    Create Message model

--- base/models.py
+++ base/models.py
@@ -1,6 +1,6 @@
+from tkinter import CASCADE
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Room(models.Model):
     #host = 
@@ -14,4 +14,17 @@ class Room(models.Model):
     def __str__(self):
         return self.name
 
-        
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.body[0:50]
+
+
