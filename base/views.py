382e644ce0727f0008dad2b3a36226ccfc12b082
Aydar Fayzullin <faidarik@mail.ru>
2022-01-23 16:27:45 -0500

    Create update functionality

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from django.forms import forms
 from django.shortcuts import render, redirect
 from django.template import context
 from .models import Room
@@ -31,3 +32,17 @@ def createRoom(request):
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
