e413303d2c57e0e64b2e4ce90e700be1091501f4
Aydar Fayzullin <faidarik@mail.ru>
2022-01-23 14:06:09 -0500

    Create Topic model

--- base/models.py
+++ base/models.py
@@ -2,6 +2,13 @@ from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
     #host = 
     #topic =
